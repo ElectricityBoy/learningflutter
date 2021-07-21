90bc02a56f0c4afee02c0187bb8b13da36484d43
Elder Pereira <elder-guedes@hotmail.com>
2021-07-20 23:32:38 -0300

    Acrescendando um Drawer, colocando menus e explorando rotas

--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -14,17 +14,72 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+               accountName: Text('PET Elétrica'),
+               accountEmail: Text('peteletrica@ufba.br'),
+               currentAccountPicture: ClipRRect(
+                 borderRadius: BorderRadius.circular(40),
+                 child: Image.network(
+                   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVd7X3pTp5kkb2j-JgXa7T_LtaXseJuSiikg&usqp=CAU')) ,),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Menu'),
+              subtitle: Text('Tela inicial'),
+              onTap: () {
+                print('home');
+              },
+            ),
+             ListTile(
+              leading: Icon(Icons.work),
+              title: Text('Estágio'),
+              subtitle: Text('Instruções e Processos'),
+              onTap: () {
+                Navigator.of(context).restorablePushReplacementNamed('/');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.school),
+              title: Text('Politécnica'),
+              subtitle: Text('Conheça a Faculdade'),
+              onTap: () {
+                Navigator.of(context).restorablePushReplacementNamed('/');
+              },
+            ),
+           
+            ListTile(
+              leading: Icon(Icons.chat),
+              title: Text('Fórum'),
+              subtitle: Text('Tire dúvidas e forme grupo de estudos'),
+              onTap: () {
+                Navigator.of(context).restorablePushReplacementNamed('/');
+              },
+            ),
+             ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Sair'),
+              subtitle: Text('Finalizar Sessão'),
+              onTap: () {
+                Navigator.of(context).restorablePushReplacementNamed('/');
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('PET ELÉTRICA UFBA'),
-        actions: [CustomSwitch(),],
+        actions: [
+          CustomSwitch(),
+        ],
       ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
         child: Column(
           children: [
-            Text('Contador: $counter'),
-            CustomSwitch(),
+            
           ],
         ),
       ),
--- ola_mundo/lib/login_page.dart
+++ ola_mundo/lib/login_page.dart
@@ -14,60 +14,70 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String senha = '';
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset('asserts/logo.png')),
+              Container(
+                height: 25,
+              ),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelText: 'Email', border: OutlineInputBorder()),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                onChanged: (text) {
+                  senha = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: 'Senha', border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              RaisedButton(
+                color: Color(0xFF001D7E),
+                textColor: Colors.white,
+                onPressed: () {
+                  if (email == 'peteletrica@ufba.br' && senha == '123456') {
+                    Navigator.of(context).pushReplacementNamed('/home');
+                    // MaterialPageRoute(builder: (context) => HomePage()),
+                  } else {
+                    print('login invalido');
+                  }
+                },
+                child: Text('Entrar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Image.asset('asserts/logo.png')
-                  ),
-                Container(height: 25,),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: 'Email', border: OutlineInputBorder()),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    senha = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: 'Senha', border: OutlineInputBorder()),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == 'peteletrica@ufba.br' && senha == '123456') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                      // MaterialPageRoute(builder: (context) => HomePage()),
-                    } else {
-                      print('login invalido');
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
-            ),
-          ),
-        ),
+      body: Stack(
+        children: [
+          _body()
+        ],
       ),
     );
   }
